4bc46865470faf55b74fcf58c70ac2c4d1e1455d
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-12-13 19:26:18 +0000

    update interface

--- convert/__init__.py
+++ convert/__init__.py
@@ -1,5 +1,6 @@
 from .srt_converter import SrtConverter
 from .ttml_to_srt import TtmlToSrtConverter
 from .sub_to_srt import SubToSrtConverter
+from .main import main
 
 __all__ = ["SrtConverter", "TtmlToSrtConverter", "SubToSrtConverter"]
--- shift.py
+++ shift.py
@@ -6,12 +6,13 @@ Given an srt file, shift the subtitle timestamps by a given time.
 
 import re
 from datetime import datetime, timedelta
+import argparse
 
 rng_regex = re.compile(r"(?P<start>\d\d:\d\d:\d\d,\d\d\d) --> (?P<end>\d\d:\d\d:\d\d,\d\d\d)")
 ts_regex = re.compile(r"(?P<hour>\d\d):(?P<minute>\d\d):(?P<second>\d\d),(?P<microsecond>\d\d\d)")
 ts_groups = ['hour', 'minute', 'second', 'microsecond'] # final group in srt timestamp is ms, but datetime needs microsecond
 
-def shift(fname, h=0, m=0, s=0, ms=0):
+def do_shift(fname, h=0, m=0, s=0, ms=0):
     out = ""
     t_shift = timedelta(hours=h, minutes=m, seconds=s, milliseconds=ms)
 
@@ -40,11 +41,8 @@ def _make_time(s):
 def _strftime(d):
     return d.strftime("%H:%M:%S,%f")[:-3] # microsecond back to ms
 
-if __name__ == "__main__":
-    
-    import argparse
-    
-    parser = argparse.ArgumentParser(description=__doc__)
+def make_argparser():
+    parser = argparse.ArgumentParser(prog="subtitools shift", description=__doc__)
     parser.add_argument('file', help='srt file')
     parser.add_argument('-o', '--output', help='path to write to. '
                         'If not provided, input will be overwritten')
@@ -56,11 +54,23 @@ if __name__ == "__main__":
                         default=0, type=int)
     parser.add_argument('-ms', '--milliseconds', help='Shift milliseconds. Default is 0', 
                         default=0, type=int)
+    return parser
+
+def shift(file, output=None, hours=0, minutes=0, seconds=0, milliseconds=0):
+    if output is None:
+        output = file
     
-    args = parser.parse_args()
+    srt = do_shift(file, hours, minutes, seconds, milliseconds)
     
-    srt = shift(args.file, args.hours, args.minutes, args.seconds, args.milliseconds)
-    ofile = args.output if args.output is not None else args.file
-    with open(ofile, 'w') as fileobj:
+    with open(output, 'w') as fileobj:
         fileobj.write(srt)
-        
+    
+def main(*args):
+    parser = make_argparser()
+    args = parser.parse_args(args)
+    shift(**vars(args))
+
+if __name__ == "__main__":
+
+    import sys
+    main(sys.argv[1:])
